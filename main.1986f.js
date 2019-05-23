f052fd09d72370ac3ebbd6a1131a6d90f90bb241
zhc921106 <184718018@qq.com>
2019-05-23 11:40:40 +0800

    22222

--- main.1986f.js
+++ main.1986f.js
@@ -144,7 +144,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.f0c8b.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.823d2.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -180,7 +180,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.945d0.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.1986f.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -207,12 +207,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.a327a.js');
+        require('src/settings.2d448.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.a327a.js');
+        require('src/settings.2d448.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
